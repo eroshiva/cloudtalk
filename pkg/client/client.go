99ecfd735cb2aa29b6fb0b62db24c921ffb2d589
Ivan Eroshkin <erijj4@gmail.com>
2026-01-18 20:45:52 +0100

    Seeding CRUD for Review resource

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/eroshiva/cloudtalk/internal/ent"
 	"github.com/eroshiva/cloudtalk/internal/ent/product"
+	"github.com/eroshiva/cloudtalk/internal/ent/review"
 	"github.com/eroshiva/cloudtalk/pkg/logger"
 	"github.com/google/uuid"
 )
@@ -17,6 +18,7 @@ const (
 
 var zlog = logger.NewLogger("db-client")
 
+// CreateProduct creates Product resource.
 func CreateProduct(ctx context.Context, client *ent.Client, name, description, price string) (*ent.Product, error) {
 	// input parameters sanity check
 	if name == "" {
@@ -53,6 +55,7 @@ func CreateProduct(ctx context.Context, client *ent.Client, name, description, p
 	return p, nil
 }
 
+// GetProductByID retrieves Product resource by its ID.
 func GetProductByID(ctx context.Context, client *ent.Client, id string) (*ent.Product, error) {
 	zlog.Debug().Msgf("Retrieving product by ID (%s)", id)
 	p, err := client.Product.
@@ -68,6 +71,7 @@ func GetProductByID(ctx context.Context, client *ent.Client, id string) (*ent.Pr
 	return p, nil
 }
 
+// EditProduct updates all provided non-nil fields in Product resource.
 func EditProduct(ctx context.Context, client *ent.Client, id string, name, description, price string) (*ent.Product, error) {
 	zlog.Debug().Msgf("Editing product (%s)", id)
 
@@ -105,6 +109,7 @@ func EditProduct(ctx context.Context, client *ent.Client, id string, name, descr
 	return p, nil
 }
 
+// ListProducts retrieves all Products available in the system.
 func ListProducts(ctx context.Context, client *ent.Client) ([]*ent.Product, error) {
 	zlog.Debug().Msgf("Retrieving all products")
 
@@ -119,11 +124,139 @@ func ListProducts(ctx context.Context, client *ent.Client) ([]*ent.Product, erro
 	return ps, nil
 }
 
+// DeleteProductByID removes Product resource with provided ID from the DB.
 func DeleteProductByID(ctx context.Context, client *ent.Client, id string) error {
 	zlog.Debug().Msgf("Deleting product with ID (%s)", id)
 	_, err := client.Product.Delete().Where(product.ID(id)).Exec(ctx)
 	if err != nil {
 		zlog.Err(err).Msgf("Failed to delete product with ID (%s)", id)
+		return err
+	}
+	return nil
+}
+
+// CreateReview creates a Review resource.
+func CreateReview(ctx context.Context, client *ent.Client, name, lastName, text string, rating int32, product *ent.Product) (
+	*ent.Review, error,
+) {
+	// input parameters sanity check
+	if name == "" {
+		err := fmt.Errorf("reviewer's name is not specified")
+		zlog.Error().Err(err).Send()
+		return nil, err
+	}
+	if lastName == "" {
+		err := fmt.Errorf("reviewer's last name is not specified")
+		zlog.Error().Err(err).Send()
+		return nil, err
+	}
+	if text == "" {
+		err := fmt.Errorf("text of the review is not specified")
+		zlog.Error().Err(err).Send()
+		return nil, err
+	}
+	if rating < 1 || rating > 5 {
+		err := fmt.Errorf("review's rating is out of range")
+		zlog.Error().Err(err).Msgf("Review's rating must be between 1 and 5, but has %d", rating)
+		return nil, err
+	}
+
+	zlog.Debug().Msgf("Creating review by %s %s for product with ID (%s)", name, lastName, product.ID)
+
+	// generating random ID for the Review resource
+	id := reviewPrefix + uuid.NewString()
+
+	p, err := client.Review.Create().
+		SetID(id).
+		SetFirstName(name).
+		SetLastName(lastName).
+		SetReviewText(text).
+		SetRating(rating).
+		SetProduct(product).
+		Save(ctx)
+	if err != nil {
+		zlog.Err(err).Msgf("Failed to create review by %s %s for product with ID (%s)", name, lastName, product.ID)
+		return nil, err
+	}
+
+	return p, nil
+}
+
+// GetReviewByID retrieves Review resource by its ID.
+func GetReviewByID(ctx context.Context, client *ent.Client, id string) (*ent.Review, error) {
+	zlog.Debug().Msgf("Retrieving review by ID (%s)", id)
+	r, err := client.Review.Query().
+		Where(review.ID(id)).
+		WithProduct(). // eager-loading edge
+		Only(ctx)
+	if err != nil {
+		zlog.Err(err).Msgf("Failed to retrieve review by ID (%s)", id)
+		return nil, err
+	}
+	return r, nil
+}
+
+// GetReviewsByProductID retrieves review resource by provided Product resource ID.
+func GetReviewsByProductID(ctx context.Context, client *ent.Client, id string) ([]*ent.Review, error) {
+	zlog.Debug().Msgf("Retrieving all reviews for Product with ID (%s)", id)
+	rs, err := client.Review.Query().
+		Where(review.HasProductWith(product.ID(id))).
+		All(ctx)
+	if err != nil {
+		zlog.Err(err).Msgf("Failed to retrieve all reviews for Product with ID (%s)", id)
+		return nil, err
+	}
+	return rs, nil
+}
+
+// EditReview updates all provided non-nil fields of Review resource.
+func EditReview(ctx context.Context, client *ent.Client, id string, name, lastName, text string, rating int32) (*ent.Review, error) {
+	zlog.Debug().Msgf("Editing review (%s)", id)
+	r, err := GetReviewByID(ctx, client, id)
+	if err != nil {
+		return nil, err
+	}
+	if name != "" {
+		r.FirstName = name
+	}
+	if lastName != "" {
+		r.LastName = lastName
+	}
+	if text != "" {
+		r.ReviewText = text
+	}
+	if rating >= 1 && rating <= 5 {
+		r.Rating = rating
+	}
+	// product is not allowed to be manipulated
+
+	numAfNodes, err := client.Review.Update().
+		Where(review.ID(id)).
+		SetFirstName(r.FirstName).
+		SetLastName(r.LastName).
+		SetReviewText(r.ReviewText).
+		SetRating(rating).
+		Save(ctx)
+	if err != nil {
+		zlog.Err(err).Msgf("Failed to edit review")
+		return nil, err
+	}
+
+	if numAfNodes != 1 {
+		// something bad has happened, returning error
+		newErr := fmt.Errorf("update of review didn't return error, number of affected nodes is %d", numAfNodes)
+		zlog.Error().Err(newErr).Send()
+		return nil, err
+	}
+	return r, nil
+}
+
+// DeleteReviewByID removes Review resource with provided ID from the DB.
+func DeleteReviewByID(ctx context.Context, client *ent.Client, id string) error {
+	zlog.Debug().Msgf("Deleting review with ID (%s)", id)
+	_, err := client.Review.Delete().Where(review.ID(id)).Exec(ctx)
+	if err != nil {
+		zlog.Err(err).Msgf("Failed to delete review with ID (%s)", id)
 		return err
 	}
 	return nil
